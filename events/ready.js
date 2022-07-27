f0f341feb04e4a0fc0ca47443f132b9cb22fa37a
RageOfFire <buihongson453@gmail.com>
2022-07-27 21:16:54 +0700

    add status

--- events/ready.js
+++ events/ready.js
@@ -3,9 +3,8 @@ module.exports = {
     run: async(bot) => {
         const { client } = bot;
         
-        bot.client.user.setActivity("CrystalGem", {
-            type: "PLAYING",
-            url: "https://www.crystalgem.cf/"
+        bot.client.user.setActivity("/help", {
+            type: "LISTENING",
         });
         client.application.commands.set([...client.slashcommands.values()])
         console.log("logged as " + bot.client.user.tag)
